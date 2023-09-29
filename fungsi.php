6651459f57e1af5648c69b0ac6958cfe09734ff0
timotiustegar06642 <112202106642@mhs.dinus.ac.id>
2023-09-29 08:42:21 +0700

    fungsi dekripsi

--- fungsi.php
+++ fungsi.php
@@ -1,6 +1,7 @@
 <?php
 //membuat koneksi ke database mysql
 $koneksi=mysqli_connect('192.168.10.253','a122106642','polke001','a122106642');
+
 function enkripsiurl($id){
     $enc = base64_encode(rand() * strtotime(date("H:i:s"))."-".$id);
     return $enc;
@@ -8,6 +9,15 @@ function enkripsiurl($id){
 function dekripsiurl($string){
     $kode = base64_decode($string);
     $id = explode("-", $kode);
-    return $id[1];
+    if(isset($id[1])){
+        return $id[1];
+    } else {
+        echo "<script>
+                alert('NPP yang Diinput Sudah Ada')
+                javascript:history.go(-1)
+        
+            </script>";
+    }
+    
 }
 ?>
