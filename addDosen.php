cfeeb65fbea510a10de89521f8c17c093bbdb876
timotiustegar06642 <112202106642@mhs.dinus.ac.id>
2023-09-29 07:48:33 +0700

    tst

--- addDosen.php
+++ addDosen.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<title>Sistem Informasi Akademik::Tambah Data Dosen</title>
+	<title>Sistem Informastesti Akademik::Tambah Data Dosen</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="bootstrap4/css/bootstrap.css">
