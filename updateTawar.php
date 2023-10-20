39413c52c6f475a34c2a6dc39735318e35983df3
timotiustegar06642 <112202106642@mhs.dinus.ac.id>
2023-10-20 08:26:45 +0700

    test

--- updateTawar.php
+++ updateTawar.php
@@ -36,7 +36,7 @@
 	$hasil = cari("kultawar", "idkultawar like'%$cari%' or idmatkul like '%$cari%' or idmatkul like '%$cari%' or npp like '%$cari%' or klp like '%$cari%' or hari like '%$cari%' or jamkul like '%$cari%' or ruang like '%$cari%'", 1, $cari);
 	?>
 	<div class="utama">
-		<h2 class="text-center mt-3">Daftar Penawaran Mata Kuliah</h2>
+		<h2 class="text-center mt-3">Daftar Penawaran Mata test</h2>
 		<!-- <div class="text-center"><a href="prnDosenPdf.php"><span class="fas fa-print">&nbsp;Print</span></a></div> -->
 		<!-- <div class="text-center"><a href="prnDosenPdf.php"><span class="fas fa-print">&nbsp;Print</span></a></div> -->
 		<span class="float-left">
